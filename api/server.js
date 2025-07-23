757ac5925aafbdda5830b01243836f5b3095e0ef
abdurwebdev <iabdurrehman22.0@gmail.com>
2025-07-23 23:32:18 +0500

    Initial commit

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const db = require('./config/db');
+const cors = require('cors');
 const userModel = require('./models/user');
 db();
 require('dotenv').config();
+app.use(cors({
+  origin: 'https://blogss-8yf8.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+app.use(express.json());
 app.get("/",(req,res)=>{
   res.send("hello")
 })
